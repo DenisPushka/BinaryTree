1e364dac837527e931efaa30a3508b64421d6519
Denis <bardenbmw@gmail.com>
2022-05-15 12:13:16 +0400

    add print

--- BinaryTree/Program.cs
+++ BinaryTree/Program.cs
@@ -6,11 +6,18 @@ namespace BinaryTree
     {
         static void Main(string[] args)
         {
-            var arrayData = new[] {5, 15, 3, 10, 21, 20, 55};
-            var tree = new Tree<int>(12);
+            var arrayData = new int[15] {2, 72, 60, 59, 50, 11, 3, 35, 61, 74, 65, 47, 1, 12, 8};
+            // var rnd = new Random();
+            // for (var i = 0; i < 15; i++)
+            // {
+            //     arrayData[i] = rnd.Next(0, 100);
+            //     Console.Write(arrayData[i] + " ");
+            // }
+
+            var tree = new Tree<int>(arrayData[arrayData.Length / 2]);
             foreach (var i in arrayData)
                 tree.Add(i);
-            tree.Remove(20);
+            tree.Print();
         }
     }
 }
--- BinaryTree/Tree.cs
+++ BinaryTree/Tree.cs
@@ -1,16 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text;
 
 namespace BinaryTree
 {
+    public class Knot<T> where T : IComparable<T>
+    {
+        public Knot<T> Parent, Left, Right;
+        public int Level;
+        public T Val;
+    }
+
     public class Tree<T> where T : IComparable<T>
     {
         private Tree<T> _parent, _left, _right;
+        private int _level;
         private T _val;
-        private readonly List<T> _listForPrint = new();
+
+        private StringBuilder[] _array;
+        //private string[] array;
+        // private Knot<T> knot;
 
         public Tree(T val, Tree<T> parent = null)
         {
+            if (parent == null) _level = 0;
+            // knot = new Knot<T>();
             _val = val;
             _parent = parent;
         }
@@ -20,14 +34,24 @@ namespace BinaryTree
             if (val.CompareTo(_val) < 0)
             {
                 if (_left == null)
-                    _left = new Tree<T>(val, this);
+                {
+                    _left = new Tree<T>(val, this)
+                    {
+                        _level = _level + 1
+                    };
+                }
                 else
                     _left.Add(val);
             }
             else
             {
                 if (_right == null)
-                    _right = new Tree<T>(val, this);
+                {
+                    _right = new Tree<T>(val, this)
+                    {
+                        _level = _level + 1
+                    };
+                }
                 else
                     _right.Add(val);
             }
@@ -55,7 +79,7 @@ namespace BinaryTree
         {
             var curTree = tree._right != null ? tree._right : tree._left;
 
-            while (curTree._left != null) 
+            while (curTree._left != null)
                 curTree = curTree._left;
 
             var temp = curTree._val;
@@ -80,7 +104,7 @@ namespace BinaryTree
             tree._left._parent = tree._parent;
             if (tree == tree._parent._left)
                 tree._parent._left = tree._left;
-            else if (tree == tree._parent._right) 
+            else if (tree == tree._parent._right)
                 tree._parent._right = tree._left;
 
             return true;
@@ -91,7 +115,7 @@ namespace BinaryTree
             tree._right._parent = tree._parent;
             if (tree == tree._parent._left)
                 tree._parent._left = tree._right;
-            else if (tree == tree._parent._right) 
+            else if (tree == tree._parent._right)
                 tree._parent._right = tree._right;
 
             return true;
@@ -101,7 +125,7 @@ namespace BinaryTree
         {
             var curTree = tree._right;
 
-            while (curTree._left != null) 
+            while (curTree._left != null)
                 curTree = curTree._left;
 
             if (curTree._parent == tree)
@@ -111,12 +135,12 @@ namespace BinaryTree
                 curTree._parent = tree._parent;
                 if (tree == tree._parent._left)
                     tree._parent._left = curTree;
-                else if (tree == tree._parent._right) 
+                else if (tree == tree._parent._right)
                     tree._parent._right = curTree;
 
                 return true;
             }
-            
+
             if (curTree._right != null)
                 curTree._right._parent = curTree._parent;
 
@@ -126,15 +150,15 @@ namespace BinaryTree
             tree._left._parent = curTree;
             tree._right._parent = curTree;
             curTree._parent = tree._parent;
-            
+
             if (tree == tree._parent._left)
                 tree._parent._left = curTree;
-            else if (tree == tree._parent._right) 
+            else if (tree == tree._parent._right)
                 tree._parent._right = curTree;
 
             return true;
         }
-        
+
         public bool Remove(T val)
         {
             var tree = Search(val);
@@ -164,19 +188,87 @@ namespace BinaryTree
             return false;
         }
 
+        private int _allLevel;
+
+        private void ScoreLevel(Tree<T> node)
+        {
+            if (node._left != null)
+            {
+                ScoreLevel(node._left);
+                if (node._right != null)
+                    ScoreLevel(node._right);
+                else
+                    CheckInequality(node);
+            }
+            else if (node._right != null)
+                ScoreLevel(node._right);
+            else
+                CheckInequality(node);
+        }
+
+        private int CheckInequality(Tree<T> node) => _allLevel > node._level ? _allLevel : _allLevel = node._level;
+
         private void Print(Tree<T> node)
         {
             if (node == null) return;
-            Print(node._left);
-            _listForPrint.Add(node._val);
-            Console.Write(node + " ");
-            if (node._right != null)
-                Print(node._right);
+            for (var i = 0; i < _array.Length; i++)
+            {
+                var buff = _array.Length - i * 0.75;
+                var split = _array[i].ToString().Split();
+                for (var f = 0; f < split.Length - 1; f++)
+                {
+                    for (var j = 0; j < Math.Pow(2, buff) - i; j++)
+                        Console.Write(' ');
+                    Console.Write(split[f]);
+                    if (f % 2 != 0) Console.Write('|');
+                }
+
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+        }
+
+        private void FillArray(Tree<T> node)
+        {
+            _array[node._level].Append(node._val).Append(' ');
+            if (node._left == null && node._right == null)
+            {
+                FillGaps(node);
+                FillGaps(node);
+            }
+            else if (node._left == null)
+                FillGaps(node);
+
+            if (node._left != null)
+            {
+                FillArray(node._left);
+                if (node._right != null)
+                    FillArray(node._right);
+            }
+            else if (node._right != null)
+                FillArray(node._right);
+
+            if (node._right == null && node._left != null)
+                FillGaps(node);
+        }
+
+        private void FillGaps(Tree<T> node)
+        {
+            for (int i = node._level + 1, up = 0; i < _allLevel + 1; i++, up++)
+            {
+                for (var j = 0; j < Math.Pow(2, up); j++)
+                    _array[i].Append("- ");
+            }
         }
 
         public void Print()
         {
-            _listForPrint.Clear();
+            ScoreLevel(this);
+            _array = new StringBuilder[_allLevel + 1];
+            for (var i = 0; i < _allLevel + 1; i++)
+                _array[i] = new StringBuilder();
+
+            FillArray(this);
             Print(this);
             Console.WriteLine();
         }
